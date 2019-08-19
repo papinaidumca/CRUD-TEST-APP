bfa6862584cc6629ebebf9eb0bbf643d9ab47dc0
Naidu <Naidu@192.168.100.11>
2019-08-20 00:04:57 +0530

    Automatically Generate numbers are created....

--- CRUD-APP/src/main/java/com/aarvee/TestClass.java
+++ CRUD-APP/src/main/java/com/aarvee/TestClass.java
@@ -1,11 +1,30 @@
 package com.aarvee;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class TestClass {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("tested the application");
-		System.out.println("tested the application");
+		//int [] numbers = new int[10];
+		int n=20;
+		for(int i=1;i<=60;i++)
+		{
+			int x= 1+(int)(Math.random()*100);
+			if(i<=n)
+			{
+			   System.out.print(x+" ");
+			}
+			else
+			{
+				System.out.println();
+				n=n+20;
+				i--;
+				
+			}
+		}
+		
 	}
 
 }
